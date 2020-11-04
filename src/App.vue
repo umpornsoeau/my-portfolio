5ca661deed70ad17386e0ae272fc36e32790bee4
umpornsoeau <umpornsoeau@protonmail.com>
2020-11-04 12:43:28 +0000

    test deploy helloworld

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Oh, here is the Hello World"/>
   </div>
 </template>
 
